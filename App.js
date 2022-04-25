b271df2fdcbbf5a680a2559a5e4b976ce90d3157
Daphne Levy <daphne@M-JL4TJJHCFJ.local>
2022-04-25 17:32:19 +0300

    fbAuth

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
-import React, {Component, useState } from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, {useEffect, useState } from 'react';
+import { Text, SafeAreaView} from 'react-native';
 import {
   LoginButton,
   AccessToken,
-  GraphRequest,
-  GraphRequestManager,
 } from 'react-native-fbsdk-next';
+import {Settings} from 'react-native-fbsdk-next';
+import Realm from 'realm';
+
 import app from './realmApp';
-import Realm, { User } from 'realm';
 
 
 
 const App = () => {
+
+  useEffect(() => {
+    Settings.setAdvertiserTrackingEnabled(true);
+    Settings.initializeSDK();
+  }, []);
+
   const [userInfo, setUser] = useState(null);
   let accessToken;
   const getFacebookAccessToken = async () => {
     let data;
-
     try {
       data = await AccessToken.getCurrentAccessToken();
     } catch (error) {
       console.log(error);
     }
-
     return data.accessToken.toString();
   };
   return (
@@ -36,19 +40,17 @@ const App = () => {
           } else {
             // Get the access token from a client application using the Facebook SDK
             accessToken = await getFacebookAccessToken();
-            // console.log(accessToken, 'accessToken')
             // Log the user in to your app
             const credentials = Realm.Credentials.facebook(accessToken);
             app.logIn(credentials).then(user => {
               setUser(user)
-              console.log(`Logged in with id: ${user}`);
             });
           }
         }}
-        onLogoutFinished={() => setUser(null)
+        onLogoutFinished={() =>  setUser(null)
         }
       />
-              {userInfo ? (
+          {userInfo ? (
           <Text style={{fontSize: 16, marginVertical: 16}}>
             You are logged in 
           </Text>
