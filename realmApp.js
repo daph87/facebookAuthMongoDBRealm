71d5081cadcd250d7d2a0d8371cc36c5c8c451d3
daph87 <52703134+daph87@users.noreply.github.com>
2022-04-25 18:03:07 +0300

    Update realmApp.js

--- realmApp.js
+++ realmApp.js
@@ -4,5 +4,4 @@ const app= new Realm.App({id : "<your Realm app ID here>"});
 
 
 // Invokes the shared instance of the Realm app.
-// TODO: Create a Realm App instance with your Realm app ID.
 export default app;
